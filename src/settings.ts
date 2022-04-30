6c8c16bc8357683c5aeefc5f42572c0feea0494f
banboobee <98196664+banboobee@users.noreply.github.com>
2022-05-01 04:26:42 +0900

    Fixed broken characters.

--- src/settings.ts
+++ src/settings.ts
@@ -277,7 +277,7 @@ export type deviceStatus = {
   shaking?: boolean;
   //only available for Smart Fan devices. the fan's swing direciton.
   shakeCenter?: string;
-  //only available for Smart Fan devices. the fan's swing range, 0~120Åã.
+  //only available for Smart Fan devices. the fan's swing range, 0~120¬∞.
   shakeRange?: string;
   //only available for Motion Sensor, Contact Sensor devices. determines if motion is detected
   moveDetected?: boolean;
