45d4bf350e8d38d93565421f79660f01e7ead5e5
Donavan Becker <beckersmarthome@icloud.com>
2025-12-16 15:21:57 -0600

    Add Presence Sensor Support

--- src/device/hub.ts
+++ src/device/hub.ts
@@ -3,7 +3,7 @@
  * hub.ts: @switchbot/homebridge-switchbot.
  */
 import type { CharacteristicValue, PlatformAccessory, Service } from 'homebridge'
-import type { device, hub2ServiceData, hub3ServiceData, hub2Status, hub2WebhookContext, SwitchBotBLE } from 'node-switchbot'
+import type { device, hub2ServiceData, hub2Status, hub2WebhookContext, hub3ServiceData, SwitchBotBLE } from 'node-switchbot'
 
 import type { SwitchBotPlatform } from '../platform.js'
 import type { devicesConfig, hubConfig } from '../settings.js'
@@ -296,8 +296,8 @@ export class Hub extends deviceBase {
         // Start to monitor advertisement packets
         const serviceData = await this.monitorAdvertisementPackets(switchBotBLE) as hub2ServiceData | hub3ServiceData
         // Update HomeKit
-        if ((serviceData.model === SwitchBotBLEModel.Hub2 && serviceData.modelName === SwitchBotBLEModelName.Hub2) || 
-            (serviceData.model === SwitchBotBLEModel.Hub3 && serviceData.modelName === SwitchBotBLEModelName.Hub3)) {
+        if ((serviceData.model === SwitchBotBLEModel.Hub2 && serviceData.modelName === SwitchBotBLEModelName.Hub2)
+          || (serviceData.model === SwitchBotBLEModel.Hub3 && serviceData.modelName === SwitchBotBLEModelName.Hub3)) {
           this.serviceData = serviceData
           if (serviceData !== undefined || serviceData !== null) {
             await this.BLEparseStatus()
